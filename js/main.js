8a284731dc0b0b5791d06fb2f77f30348a2196fb
Nicolas Lemus <nicolaswaldemarlemuscurbelo@gmail.com>
2023-03-15 19:51:55 -0300

    correcciones de texto de menu

--- js/main.js
+++ js/main.js
@@ -44,10 +44,10 @@ function ingresoExitoso() {
     switch (opciones) {
       //Opciones de Calzado
       case "1":
-        let selectCalzado = prompt("Elija tipo de calzado:\n1. Deportivo\n2. Casual\n3. Formal\n4. Volver al menu principal");
+        let selectCalzado = prompt("Elija tipo de calzado:\n1. Deportivo\n2. Casual\n3. Formal\n4. Volver");
         switch(selectCalzado){
           case "1":
-            let selectDeportivo=prompt("\n1. Nike-$4000 \n2. Adidas-$3900 \n3. Under-$5000 \n4. Volver");
+            let selectDeportivo=prompt("\n1. Nike-$4000 \n2. Adidas-$3900 \n3. Under-$5000 \n4. Volver al menu principal");
             switch(selectDeportivo){ 
               case"1":
                 saldo -= 4000;
@@ -67,7 +67,7 @@ function ingresoExitoso() {
             break;
             //Opciones de Casual
             case"2":
-            let selectCasual=prompt("\n1. Botas-$5000 \n2. Nuticas-$4900 \n3. Oxford-$6000 \n4. Volver")
+            let selectCasual=prompt("\n1. Botas-$5000 \n2. Nuticas-$4900 \n3. Oxford-$6000 \n4. Volver al menu principal")
             switch(selectCasual){
               case"1":
                 saldo -= 5000;
@@ -87,7 +87,7 @@ function ingresoExitoso() {
             break;
             //Opciones de Formal
           case "3":
-            let selectFormal=prompt("\n1. Cuero-$7000 \n2. Zapatos sin cordones-$5900 \n3. Desert-$6900 \n4. Volver")
+            let selectFormal=prompt("\n1. Cuero-$7000 \n2. Zapatos sin cordones-$5900 \n3. Desert-$6900 \n4. Volver al menu principal")
             switch(selectFormal){
               case"1":
                 saldo -= 7000;
@@ -111,11 +111,11 @@ function ingresoExitoso() {
         } break;
         //Opciones de Prendas 
       case "2":
-        let selectPrenda = prompt("Elija tipo de prenda:\n1. Invierno\n2. Verano\n3. Primavera\n4. Volver al menu principal");
+        let selectPrenda = prompt("Elija tipo de prenda:\n1. Invierno\n2. Verano\n3. Primavera\n4. Volver");
         switch(selectPrenda){
           case "1":
             //Opciones de Invierno
-            let selectInvierno=prompt("\n1. Gorro-$1000 \n2. Guantes-$900 \n3. Campera-$5000 \n4. Volver");
+            let selectInvierno=prompt("\n1. Gorro-$1000 \n2. Guantes-$900 \n3. Campera-$5000 \n4. Volver al menu principal");
             switch(selectInvierno){ 
               case"1":
                 saldo -= 1000;
@@ -135,7 +135,7 @@ function ingresoExitoso() {
             break;
             //Opciones de Verano
             case"2":
-            let selectVerano=prompt("\n1. Remera-$2000 \n2. Short-$2990 \n3. Gorra-$1090 \n4. Volver")
+            let selectVerano=prompt("\n1. Remera-$2000 \n2. Short-$2990 \n3. Gorra-$1090 \n4. Volver al menu principal")
             switch(selectVerano){
               case"1":
                 saldo -= 2000;
@@ -155,7 +155,7 @@ function ingresoExitoso() {
             break;
             //Opciones de Primavera 
           case "3":
-            let selectPrimavera=prompt("\n1. Remera manga larga-$2390 \n2. Jeans-$3990 \n3. Campera fina-$4900 \n4. Volver")
+            let selectPrimavera=prompt("\n1. Remera manga larga-$2390 \n2. Jeans-$3990 \n3. Campera fina-$4900 \n4. Volver al menu principal")
             switch(selectPrimavera){
               case"1":
                 saldo -= 2390;
@@ -179,11 +179,11 @@ function ingresoExitoso() {
         } break;
         //Opciones de Accesorios
         case "3":
-          let selectAccesorio = prompt("Elija tipo de Accesorio:\n1. Reloj\n2. Anillo\n3. Cadenita\n4. Volver al menu principal");
+          let selectAccesorio = prompt("Elija tipo de Accesorio:\n1. Reloj\n2. Anillo\n3. Cadenita\n4. Volver");
           switch(selectAccesorio){
         case "1":
           // Opciones de Reloj
-          let selectReloj=prompt("\n1. Malla Acero-$7000\n2. Malla Cuero-$5900 \n3. Inteligente-$5000 \n4. Volver");
+          let selectReloj=prompt("\n1. Malla Acero-$7000\n2. Malla Cuero-$5900 \n3. Inteligente-$5000 \n4. Volver al menu principal");
           switch(selectReloj){ 
             case"1":
               saldo -= 7000;
@@ -203,7 +203,7 @@ function ingresoExitoso() {
           break;
           //Opciones de Anillo
         case"2":
-          let selectAnillo=prompt("\n1. Oro-$12.000 \n2. Plata-$7490 \n3. Bronce-$5190 \n4. Volver")
+          let selectAnillo=prompt("\n1. Oro-$12.000 \n2. Plata-$7490 \n3. Bronce-$5190 \n4. Volver al menu principal")
           switch(selectAnillo){
             case"1":
               saldo -= 12000;
@@ -223,7 +223,7 @@ function ingresoExitoso() {
           break;
           //Opciones de Cadenita
         case "3":
-          let selectCadenita=prompt("\n1. Cadena 18k-$13.390 \n2. Bañada en oro-$9999 \n3.Plata de ley 925 -$8250 \n4. Volver")
+          let selectCadenita=prompt("\n1. Cadena 18k-$13.390 \n2. Bañada en oro-$9999 \n3.Plata de ley 925 -$8250 \n4. Volver al menu principalgit ")
           switch(selectCadenita){
             case"1":
               saldo -= 13390;
